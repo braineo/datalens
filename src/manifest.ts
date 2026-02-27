e68868c033d2249038596fcb4ddd861c01ea67c8
Binbin Ye <binbin.ye@mujin.co.jp>
2026-02-27 14:56:47 +0900

    fix: remove unnecessary permissions

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,9 +3,15 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "DataLens",
-  description: "Extract EOF embedded JSON from images",
+  description: "Extract and view embedded JSON data directly from images on any webpage.",
   version: "1.0.0",
-  permissions: ["contextMenus", "activeTab", "scripting"],
+  icons: {
+    "16": "icons/icon-16.png",
+    "32": "icons/icon-32.png",
+    "48": "icons/icon-48.png",
+    "128": "icons/icon-128.png",
+  },
+  permissions: ["contextMenus"],
   background: {
     service_worker: "src/background/index.ts",
     type: "module",
